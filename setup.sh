bb8f686bd1f88736ac4b4181dc07ed4a2c10910e
AlxFrst <alexandreforestier1107@gmail.com>
2023-06-30 14:02:06 +0200

    Oops

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt install curl software-properties-common -y
 echo "Installation de Docker 🐳 et Docker Compose 🐙"
 sudo apt install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL --force https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
