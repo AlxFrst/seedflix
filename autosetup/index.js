4a88522a2274e6ec06f3551bc71bf0064a9f6c96
AlxFrst <alexandreforestier1107@gmail.com>
2023-07-04 23:58:23 +0200

    try do add default index need to handle errors...

--- autosetup/index.js
+++ autosetup/index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-let myIP = '192.168.1.128'; // change for debug test with a local server
+let myIP = '192.168.1.210'; // change for debug test with a local server
 let qBittorrentUrl = 'http://' + myIP + ':8080';
 let jackettUrl = 'http://' + myIP + ':9117';
 let sonarrUrl = 'http://' + myIP + ':8989';
@@ -16,62 +16,59 @@ function delay(time) {
 
 (async () => {
 
-
     const browser = await puppeteer.launch({ headless: false });
 
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 }); // DEBUG
-
+    const qBittorrentpage = await browser.newPage();
+    await qBittorrentpage.setViewport({ width: 1920, height: 1080 }); // DEBUG
     // qBittorrent config
     let qbUsername = 'admin';
     let qbPassword = 'adminadmin';
     let qbDownloadPath = '/data/downloads'; // NEED TO SED THIS BEFORE RUNNING
 
-    await page.goto(qBittorrentUrl);
-    await page.type('#username', qbUsername);
-    await page.type('#password', qbPassword);
-    await page.click('#login');
-    await page.waitForSelector('#torrentsTableDiv');
-    await page.hover('#desktopNavbar > ul > li:nth-child(4)')
-    await page.click('#preferencesLink');
-    await page.click('#savepath_text');
-    await page.keyboard.down('Control');
-    await page.keyboard.press('A');
-    await page.keyboard.up('Control');
-    await page.keyboard.press('Backspace');
-    await page.type('#savepath_text', qbDownloadPath);
-    await page.click('#PrefBittorrentLink > a');
-    await page.click('#max_ratio_checkbox');
-    await page.click('#max_ratio_value');
-    await page.keyboard.down('Control');
-    await page.keyboard.press('A');
-    await page.keyboard.up('Control');
-    await page.keyboard.press('Backspace');
-    await page.type('#max_ratio_value', '0');
-    await page.click('#preferencesPage_content > div:nth-child(8) > input[type=button]')
+    await qBittorrentpage.goto(qBittorrentUrl, { waitUntil: 'networkidle2' });
+    await qBittorrentpage.type('#username', qbUsername);
+    await qBittorrentpage.type('#password', qbPassword);
+    await qBittorrentpage.click('#login');
+    await qBittorrentpage.waitForSelector('#torrentsTableDiv');
+    await qBittorrentpage.hover('#desktopNavbar > ul > li:nth-child(4) > a');
+    await qBittorrentpage.waitForSelector('#preferencesLink');
+    await qBittorrentpage.click('#preferencesLink');
+    await qBittorrentpage.waitForSelector('#PrefDownloadsLink > a');
+    await qBittorrentpage.click('#PrefDownloadsLink > a');
+    await qBittorrentpage.waitForSelector('#savepath_text');
+    // replace download path
+    await qBittorrentpage.evaluate((qbDownloadPath) => {
+        document.querySelector('#savepath_text').value = qbDownloadPath;
+    }, qbDownloadPath);
+    await qBittorrentpage.click('#PrefBittorrentLink > a');
+    await qBittorrentpage.waitForSelector('#max_ratio_checkbox');
+    await qBittorrentpage.click('#max_ratio_checkbox');
+    // replace #max_ratio_value with 0
+    await qBittorrentpage.evaluate(() => {
+        document.querySelector('#max_ratio_value').value = 0;
+    });
+    await qBittorrentpage.click('#preferencesPage_content > div:nth-child(8) > input[type=button]');
+    console.log('qBittorrent config done');
+    await qBittorrentpage.close();
 
     // Jackett config
-    // await page.goto(jackettUrl);
-    // await page.waitForSelector('#api-key-input');
-    // let jackettApiKey = await page.$eval('#api-key-input', el => el.value);
-    // console.log(jackettApiKey);
-    // // add indexers
-    // await delay(3000);
-    // await page.click('#jackett-add-indexer');
-    // await delay(3000);
-    // await page.click('#selecttorrent911');
-    // await page.click('#add-selected-indexers');
+    // Open new tab
+    const jackettPage = await browser.newPage();
+    await jackettPage.setViewport({ width: 1920, height: 1080 }); // DEBUG
 
-    // // for each class sorting_1 console.log find
-    // let indexerNames = []
-    // const elements = await page.$$('#configured-indexer-datatable > tbody > tr');
-    // for (const element of elements) {
-    //     let indexerName = await element.$eval('td:nth-child(1) > a', el => el.innerText);
-    //     indexerNames.push(indexerName);
-    // }
-    // console.log(indexerNames);
+    // Jackett config
+    let defaultIndexer = ['1337x', 'torrent911', 'limetorrents', 'cpasbien'];
 
-
-
+    await jackettPage.goto(jackettUrl, { waitUntil: 'networkidle2' });
+    await jackettPage.waitForSelector('#jackett-add-indexer');
+    await jackettPage.click('#jackett-add-indexer');
+    await delay(3000);
+    for (let i = 0; i < defaultIndexer.length; i++) {
+        console.log('add ' + defaultIndexer[i]);
+        await jackettPage.click('#select' + defaultIndexer[i]);
+        await delay(2000); // Attendre après chaque clic
+    }
+    await jackettPage.click('#add-selected-indexers');
+    await delay(3000);
 
 })();
