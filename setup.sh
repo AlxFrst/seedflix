a83362ed3e0c4f75a0aa0ba85c1823301ceaaab7
AlxFrst <alexandreforestier1107@gmail.com>
2023-07-07 15:42:26 +0200

    forgot to add $username

--- setup.sh
+++ setup.sh
@@ -155,10 +155,10 @@ if [ "$autosetup" = true ] ; then
     sudo apt-get install -y libx11-xcb1 libxcomposite1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6
     sudo -u $username npm install --prefix /home/$username/seedflix/autosetup
     # Start jackett.js
-    sudo -u media node /home/media/seedflix/autosetup/jackett.js && \
-    sudo -u media node /home/media/seedflix/autosetup/radarr.js && \
-    sudo -u media node /home/media/seedflix/autosetup/sonarr.js && \
-    sudo -u media node /home/media/seedflix/autosetup/qbittorrent.js
+    sudo -u $username node /home/$username/seedflix/autosetup/jackett.js && \
+    sudo -u $username node /home/$username/seedflix/autosetup/radarr.js && \
+    sudo -u $username node /home/$username/seedflix/autosetup/sonarr.js && \
+    sudo -u $username node /home/$username/seedflix/autosetup/qbittorrent.js
 
     # SED FOR JELLYFIN
     # SED FOR JELLISEERR
