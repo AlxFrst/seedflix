8e8d2165341478b46fe80ac5e8d3677acdd2702f
AlxFrst <alexandreforestier1107@gmail.com>
2023-07-07 16:09:10 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -154,8 +154,8 @@ if [ "$autosetup" = true ] ; then
     sudo apt-get install -y chromium-browser
     sudo apt-get install -y libx11-xcb1 libxcomposite1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6
     sudo -u $username npm install --prefix /home/$username/seedflix/autosetup
-    # keys.json need to be readeable by the user
-    sudo chown -R $username:$username /home/$username/seedflix/autosetup/keys.json
+    # keys.json need to be readeable by all
+    sudo chmod 777 /home/$username/seedflix/autosetup/keys.json
     sudo -u $username node /home/$username/seedflix/autosetup/jackett.js && \
     sudo -u $username node /home/$username/seedflix/autosetup/radarr.js && \
     sudo -u $username node /home/$username/seedflix/autosetup/sonarr.js && \
