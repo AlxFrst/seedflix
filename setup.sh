fde19e7129c03131d4211667da1ca740f24b2ab3
AlxFrst <alexandreforestier1107@gmail.com>
2023-07-10 08:06:01 +0200

    update

--- setup.sh
+++ setup.sh
@@ -165,11 +165,17 @@ fi
 
 # Services apps installation
 if [ "$autosetup" = true ] ; then
+    curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt update -y
+    sudo apt-get install -y nodejs
+    sudo apt-get install -y chromium-browser
+    sudo apt-get install -y libx11-xcb1 libxcomposite1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6
+    sudo -u $username npm install --prefix /home/$username/seedflix/autosetup
     echo "[AUTO-SETUP] Installation en cours 👀"
     sudo -u $username sed -i "s/#jellyuser/$jellyfinuser/g" /home/$username/seedflix/autosetup/index.js
     sudo -u $username sed -i "s/#jellypass/$jellyfinpassword/g" /home/$username/seedflix/autosetup/index.js
     sudo sed -i "s@#path#@$path@g" "/home/$username/seedflix/autosetup/index.js"
-    sudo -u $username node /home/$username/seedflix/autosetup/index.js
+    sudo -u media node /home/media/seedflix/autosetup/index.js
     else
     echo "[AUTO-SETUP] Pas d'installation automatique des services Seedflix ❌"
 fi
